2734629f469db2326a5288c8ac2e78976f3e270a
lucasfierroruiz1 <lfierror@charlotte.edu>
2025-12-01 18:58:21 -0500

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def summarize_text(text, max_sentences=3):
-    sentences = re.split(r'(?<=[.!?]) +', text)
-    return ' '.join(sentences[:max_sentences])
-
-def extract_keywords(text, keywords):
-    return [kw for kw in keywords if kw in text.lower()]
